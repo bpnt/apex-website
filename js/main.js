2e4b5b56fd3cde24b1281349b3d22f252cdfc7d0
bpnt <pant.bini@gmail.com>
2024-03-22 22:21:25 +0530

    add:Update on files

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
 
 $('.fa-bars').click(function(){
     $(this).toggleClass('fa-times');
-    $('.navbar').toggleClass('nav-toggle')
-})
+    $('.navbar').toggleClass('nav-toggle');
+});
+
+$(window).on('load scroll', function(){
+    $('.fa-bars').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+});
 
 });
